5f5ae030a66e7ad2c97126c08ec7772d184593a4
jonathanandradevalenzuela <jmv1006@gmail.com>
2021-10-21 20:01:32 -0700

    index.html

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const buttons = document.querySelectorAll("button");
+const display = document.getElementById('display');
+
+
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    console.log('click')
+  })
+});
+
+
+
+
+
