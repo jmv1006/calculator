9ec7bc6872bc200a6a6099a7dc481d9eae86c841
jonathanandradevalenzuela <jmv1006@gmail.com>
2021-10-21 21:59:02 -0700

    script.js

--- script.js
+++ script.js
@@ -5,11 +5,41 @@ const display = document.getElementById('display');
 
 buttons.forEach(button => {
   button.addEventListener('click', function () {
-    console.log('click')
+    const numb = Number(button.innerHTML);
+    displayInputArea(numb);
   })
 });
 
 
 
+function displayInputArea(num) {
+  let displayValue = num;
+  document.getElementById('inputarea').innerHTML = displayValue;
+}
+
+function add(a, b) {
+  console.log(a + b)
+}
+
+function subtract(a, b) {
+  console.log(a - b);
+}
+
+function multiply(a, b) {
+  console.log(a * b);
+}
+
+function divide(a, b) {
+  console.log(a / b);
+}
 
 
+
+function operate(a, b) {
+  add(5, 7);
+  subtract(25, 5);
+  multiply(10, 12);
+  divide(120, 11);
+}
+
+operate();
