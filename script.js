100b26cf9ccbb034802c6a538375cb29b95b64a7
jonathanandradevalenzuela <jmv1006@gmail.com>
2021-10-22 19:44:26 -0700

    script.js

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 
 const buttons = document.querySelectorAll("button");
 const display = document.getElementById('display');
-let displayValue;
+let displayValue1;
+let displayvalue2;
+let getOperatorChoice = {
+  '+': function (a, b) { return a + b },
+  '<': function (a, b) { return a < b },
+  '*': function (a, b) { return a * b },
+  '/': function (a, b) { return a / b },
+};
+let op;
+//see line 57 to see how to use above functions
 
 buttons.forEach(button => {
   button.addEventListener('click', function () {
     if (this.className == 'digit') {
-      const numb = Number(button.innerHTML);
+      let numb = Number(button.innerHTML);
       displayInputArea(numb);
     }
+    else if (this.id == 'clear') {
+      clear();
+    }
     else {
-      const operator = (button.innerHTML);
-      displayInputArea(operator);
-      //will need to call a function that stores chosen operator
+      const operatorSet = (button.innerHTML);
+      displayInputArea(operatorSet);
+      //will need to call a function that stores chosen operator ^
     }
   })
 });
 
-
-
 function displayInputArea(num) {
-  displayValue = num;
-  document.getElementById('inputarea').innerHTML = displayValue;
+  displayValue1 = num;
+  document.getElementById('inputarea').innerHTML = displayValue1;
 }
 
 function add(a, b) {
@@ -40,13 +50,12 @@ function divide(a, b) {
   console.log(a / b);
 }
 
-
+function clear() {
+  document.getElementById('inputarea').innerHTML = '';
+}
 
 function operate(a, b) {
-  add(5, 7);
-  subtract(25, 5);
-  multiply(10, 12);
-  divide(120, 11);
+  console.log(getOperatorChoice[op](displayValue1, displayvalue2));
 }
 
 operate();
